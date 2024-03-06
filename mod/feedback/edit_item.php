769efd350e2b8d97b7266d679d5c504155f735c3
Paul Holden <paulh@moodle.com>
2024-03-06 21:24:45 +0000

    MDL-81135 mod_feedback: stricter cleaning of item type parameter.

--- mod/feedback/edit_item.php
+++ mod/feedback/edit_item.php
@@ -63,12 +63,9 @@ if (!$item->id && $typ === 'pagebreak') {
 }
 
 //get the existing item or create it
-// $formdata->itemid = isset($formdata->itemid) ? $formdata->itemid : NULL;
-if (!$typ || !file_exists($CFG->dirroot.'/mod/feedback/item/'.$typ.'/lib.php')) {
-    print_error('typemissing', 'feedback', $editurl->out(false));
+if (!$typ) {
+    throw new \moodle_exception('typemissing', 'feedback', $editurl->out(false));
 }
-
-require_once($CFG->dirroot.'/mod/feedback/item/'.$typ.'/lib.php');
 
 $itemobj = feedback_get_item_class($typ);
 
